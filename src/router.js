f740fd0c753c140f68b6d7419ac1975d5696f697
marutaro1 <UsersRecord@gmail.com>
2021-07-07 13:23:15 +0900

    component User

--- src/router.js
+++ src/router.js
@@ -2,12 +2,15 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Users from './views/Users.vue';
 import NewUser from './views/NewUser.vue';
+import User from './views/User.vue';
 
 Vue.use(Router);
 
 export default new Router({
+    mode: 'history',
     routes: [
         {path: '/', component: Users},
-        {path: '/NewUser', component: NewUser}
+        {path: '/NewUser', component: NewUser},
+        {path: '/User/:id', component: User, props: true}
     ]
 });
---
+++ src/views/User.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h3>利用者</h3>
+        <p>{{ id }}</p>
+    </div>
+</template>
+<script>
+    export default {
+        props: ['id']
+    };
+</script>
