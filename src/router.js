488291316dea3e1c87a0834b3554db048ab969ae
marutaro1 <UsersRecord@gmail.com>
2021-09-01 08:02:32 +0900

    Lazyloading

--- src/router.js
+++ src/router.js
@@ -1,15 +1,27 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-import Users from './views/Users.vue';
-import NewUser from './views/NewUser.vue';
-import User from './views/User.vue';
-import Records from './views/Records.vue';
-import UpdateUser from './views/UpdateUser.vue';
-import Archives from './views/Archives.vue';
-import Manuel from './views/Manuel.vue';
-import MedicalHistory from './views/MedicalHistory.vue';
-import SignUp from './views/SignUp.vue';
+const Users = () => import(/* webpackChunkName: "Users" */ './views/Users.vue');
+const NewUser = () => import(/* webpackChunkName: "NewUser" */ './views/NewUser.vue');
+const User = () => import(/* webpackChunkName: "User" */ './views/User.vue');
+
+const Records = () => import(/* webpackChunkName: "Records" */ './views/Records.vue');
+const UpdateUser = () => import(/* webpackChunkName: "UpdateUser" */ './views/UpdateUser.vue');
+const Archives = () => import(/* webpackChunkName: "Archives" */ './views/Archives.vue');
+const Manuel = () => import(/* webpackChunkName: "Manuel" */ './views/Manuel.vue');
+const MedicalHistory = () => import(/* webpackChunkName: "MedicalHistory" */ './views/MedicalHistory.vue');
+const SignUp = () => import(/* webpackChunkName: "SignUp" */ './views/SignUp.vue');
+
+// import Users from './views/Users.vue';
+// import NewUser from './views/NewUser.vue';
+// import User from './views/User.vue';
+// import Records from './views/Records.vue';
+// import UpdateUser from './views/UpdateUser.vue';
+// import Archives from './views/Archives.vue';
+// import Manuel from './views/Manuel.vue';
+// import MedicalHistory from './views/MedicalHistory.vue';
+// import SignUp from './views/SignUp.vue';
+
 
 
 
